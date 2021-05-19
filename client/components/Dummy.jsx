72f424a62aa2493e0023023af82ee919a93311e4
m-aung <myothuwinaung1991@gmail.com>
2021-05-19 15:33:45 -0400

    login component updated

--- client/components/Dummy.jsx
+++ client/components/Dummy.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-const Dummy = () => {
+const Dummy = (props) => {
+  const handleLogout = ()=> {
+    props.history.push('/')
+  }
   return (
     <div>
-    Welcome to Dummy</div>
+    Welcome to DashBoard!
+    <br/><br/>
+    <input type='button' value= 'Logout' onClick={handleLogout}></input>
+    </div>
   )
 }
 export default Dummy;
--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-const Login = () => {
-  
+import React, {useState} from 'react'
+const Login = props => {
+
+  // Error Handling
+  // global login error
+  const [error, setError] = useState(null);
+  // is data loaded
+  const [loading, setLoading] = useState(false)
+  // username
+  const [username, setUsername] = useState('');
+  // password
+  const [password, setPassword] = useState('');
+  //onChange for username and password function
+  const handleInputChange = e => {
+    const {id, value} = e.target;
+    if(id === 'username') setUsername(value)
+    else {
+      setPassword(value)
+    }
+  }
+  //login on submit
+  const handleLogin = () => {
+    props.history.push('/dashboard')
+  }
 
   return (
     <div>
@@ -10,15 +31,16 @@ const Login = () => {
     <div>
     <label>Username</label>
     <br/>
-    <input type ='text' placeholder ='john135@gmail.com'></input>
+    <input type ='text' placeholder ='john135@gmail.com' id ='username' value ={username} onChange = {handleInputChange}></input>
     </div>
     <div>
     <label>Password</label>
     <br/>
-    <input type ='password'></input>
+    <input type ='password' id ='password'value ={password} onChange = {handleInputChange}></input>
     </div>
-    <br/>
-    <input type="button" value="Login" />
+    {error && <div className="error">{error}</div>}
+    <input type="button" value={loading ? "Loading...": "Login"} disabled={loading} onClick ={handleLogin}
+    />
     </div>
   )
 }
